fda11f72cdd6b21825c0c70832e8775f907ec655
Kinaro01 <tom.labru1@gmail.com>
2025-12-01 14:22:53 +0100

    creation base

---
+++ Base de données/sql creation base projet.sql
@@ -0,0 +1,52 @@
+-- SQLite
+CREATE TABLE profil (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+nom TEXT NOT NULL,
+liste_streamers_suivis TEXT NOT NULL,
+nombre_followers INT,
+en_live BOOL,
+est_streamer BOOL
+);
+
+CREATE TABLE calendrier (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+id_profil INT,
+nom TEXT NOT NULL,
+date INT,
+FOREIGN KEY (id_profil) REFERENCES profil(id)
+);
+
+CREATE TABLE abonnement (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+id_profil INT,
+niveau INT,
+est_abonne BOOL,
+FOREIGN KEY (id_profil) REFERENCES profil(id)
+);
+
+CREATE TABLE live (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+id_profil INT,
+nom TEXT NOT NULL,
+nombre_viewers INT,
+temps_live INT,
+historique_chat TEXT NOT NULL,
+FOREIGN KEY (id_profil) REFERENCES profil(id)
+);
+
+CREATE TABLE categorie (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+nom TEXT NOT NULL,
+nombre_viewers INT,
+nombre_followers INT
+);
+
+CREATE TABLE clips (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+id_profil INT,
+id_live INT,
+nom TEXT NOT NULL,
+date INT,
+FOREIGN KEY (id_profil) REFERENCES profil(id),
+FOREIGN KEY (id_live) REFERENCES live(id)
+);
